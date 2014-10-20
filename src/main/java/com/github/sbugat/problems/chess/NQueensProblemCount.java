c139ce00b4e36c580f9e138c3a705e96c4484923
Sylvain Bugat <sylvain.bugat@laposte.net>
2014-10-20 23:10:58 +0200

    Optimize counting N Queens algorithm

--- src/main/java/com/github/sbugat/problems/chess/NQueensProblemCount.java
+++ src/main/java/com/github/sbugat/problems/chess/NQueensProblemCount.java
@@ -39,10 +39,57 @@ public class NQueensProblemCount {
 		Arrays.fill( usedDescendingDiagonals, true );
 
 		//Start the algorithm at the fisrt line
-		solve( 0 );
+		solve();
 
 		//End of the algorithm print the total of solution(s) found
 		System.out.println( "Total number of solution(s):" + solutionCount );
+	}
+
+	/**
+	 * First line to divide by 2 explored tree
+	 */
+	private void solve() {
+
+		//Test half square of the line
+		for( int x=0 ; x < chessboardSize/2 ; x ++ ){
+
+			final int diag1 = x;
+			final int diag2 = x + chessboardSize - 1;
+
+			usedColumns[ x ] = false;
+			usedAscendingDiagonals[ diag1 ] = false;
+			usedDescendingDiagonals[ diag2 ] = false;
+
+			//Go on to the second line
+			solve( 1 );
+
+			usedDescendingDiagonals[ diag2 ] = true;
+			usedAscendingDiagonals[ diag1 ] = true;
+			usedColumns[ x ] = true;
+		}
+
+		//Multiply by 2 the solution count for the other half not calculated
+		solutionCount *= 2;
+
+		//If the cheesboard size is odd, test with a queen on the middle of the first line
+		if( 0 != chessboardSize % 2 ) {
+
+			final int x=chessboardSize/2;
+
+			final int diag1 = x;
+			final int diag2 = x + chessboardSize - 1;
+
+			usedColumns[ x ] = false;
+			usedAscendingDiagonals[ diag1 ] = false;
+			usedDescendingDiagonals[ diag2 ] = false;
+
+			//Go on to the second line
+			solve( 1 );
+
+			usedDescendingDiagonals[ diag2 ] = true;
+			usedAscendingDiagonals[ diag1 ] = true;
+			usedColumns[ x ] = true;
+		}
 	}
 
 	/**
@@ -64,7 +111,7 @@ public class NQueensProblemCount {
 				//if both diagonals are not already blocked by anothers queens
 				if( usedAscendingDiagonals[ diag1 ] && usedDescendingDiagonals[ diag2 ] ) {
 
-					usedColumns[x] = false;
+					usedColumns[ x ] = false;
 					usedAscendingDiagonals[ diag1 ] = false;
 					usedDescendingDiagonals[ diag2 ] = false;
 
@@ -106,6 +153,11 @@ public class NQueensProblemCount {
 			case 'n':
 				try {
 					chessBoardSize = Integer.parseInt( getOpt.getOptarg() );
+
+					if( chessBoardSize < 2 ) {
+						System.err.println( "Usage: " + NQueensProblemCount.class.getSimpleName() + " [-n <size of the chessboard>]" );
+						System.exit( 1 );
+					}
 				}
 				catch( final NumberFormatException e ) {
 					System.err.println( "Usage: " + NQueensProblemCount.class.getSimpleName() + " [-n <size of the chessboard>]" );
