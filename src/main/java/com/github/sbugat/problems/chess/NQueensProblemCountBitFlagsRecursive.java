ec10824b933a183d929514f1a3cb6449595f77c8
Sylvain Bugat <sylvain.bugat@laposte.net>
2014-10-22 23:02:27 +0200

    Add N Queens problem counting variant

---
+++ src/main/java/com/github/sbugat/problems/chess/NQueensProblemCountBitFlagsRecursive.java
@@ -0,0 +1,127 @@
+package com.github.sbugat.problems.chess;
+
+import gnu.getopt.Getopt;
+
+import java.io.IOException;
+
+/**
+ * Classic N chess queens on a size N chess board
+ *
+ * Time on Intel Q6600 CPU:
+ * 8       9         10       11       12       13       14       15        16
+ * 0m0.140s 0m0.126s 0m0.115s 0m0.123s 0m0.194s 0m0.575s 0m2.908s 0m18.047s 2m3.400s
+ *
+ * @author Sylvain Bugat
+ *
+ */
+public class NQueensProblemCountBitFlagsRecursive {
+
+	/**long bits flag to mark unused columns*/
+	private int unusedColumns;
+	/**long bits flag to mark unused ascending diagonals, first */
+	private long unusedAscendingDiagonals;
+	/**long bits flag to mark unused descending diagonals */
+	private long unusedDescendingDiagonals;
+	/**Number of solution counter*/
+	private long solutionCount;
+
+	/**Size of the chess board*/
+	private final int chessboardSize;
+
+	/**bits flag shifting for diagonals bits flags*/
+	private final int diagonalShifting;
+
+	public NQueensProblemCountBitFlagsRecursive( final int chessboardSizeArg ) {
+
+		chessboardSize = chessboardSizeArg;
+		diagonalShifting = 32 - chessboardSizeArg / 2;
+
+		//Start the algorithm at the fisrt line
+		solve( 0 );
+
+		//End of the algorithm print the total of solution(s) found
+		System.out.println( "Total number of solution(s):" + solutionCount );
+	}
+
+	/**
+	 * Solving recursive method, do a depth-first/back-tracking algorithm with bit flags to mark used columns and diagonals
+	 *
+	 * @param y number of the line stating at 0
+	 */
+	private void solve( final int y ) {
+
+		//Test all square of the line
+		for( int x=0 ; x < chessboardSize ; x ++ ){
+
+			//if the row is not already blocked by another queen
+			if( ( unusedColumns & ( 1L << x ) ) == 0 ) {
+
+				//if both diagonals are not already blocked by anothers queens
+				if( ( unusedAscendingDiagonals & ( 1L << diagonalShifting + x ) ) == 0 && ( unusedDescendingDiagonals & ( 1L << diagonalShifting + x ) ) == 0 ) {
+
+					unusedColumns = unusedColumns | ( 1 << x );
+					unusedAscendingDiagonals = ( unusedAscendingDiagonals | ( 1L << diagonalShifting + x ) ) << 1;
+					unusedDescendingDiagonals = ( unusedDescendingDiagonals | ( 1L << diagonalShifting + x ) ) >> 1;
+
+					//All queens are sets on the chessboard then a solution is found!
+					if( y + 1 >= chessboardSize ) {
+						solutionCount++;
+					}
+					else {
+						//Go on to the next line
+						solve( y + 1 );
+					}
+
+					unusedColumns = unusedColumns ^ ( 1 << x );
+					unusedAscendingDiagonals = unusedAscendingDiagonals >> 1 ^ ( 1L << diagonalShifting + x );
+					unusedDescendingDiagonals = unusedDescendingDiagonals << 1 ^ ( 1L << diagonalShifting + x );
+				}
+			}
+		}
+	}
+
+	/**
+	 *
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main( final String args[] ) {
+
+		final Getopt getOpt = new Getopt( "NQueensProblem", args, ":n:" );
+		getOpt.setOpterr( false );
+
+		//Default chessboard size
+		int chessBoardSize = 8;
+
+		int c = getOpt.getopt();
+		while( -1 != c )
+		{
+			switch(c)
+			{
+			case 'n':
+				try {
+					chessBoardSize = Integer.parseInt( getOpt.getOptarg() );
+
+					if( chessBoardSize < 2 || chessBoardSize > 31 ) {
+						System.err.println( "Usage: " + NQueensProblemCountBitFlagsRecursive.class.getSimpleName() + " [-n <size of the chessboard>]" );
+						System.exit( 1 );
+					}
+				}
+				catch( final NumberFormatException e ) {
+					System.err.println( "Usage: " + NQueensProblemCountBitFlagsRecursive.class.getSimpleName() + " [-n <size of the chessboard>]" );
+					System.exit( 1 );
+				}
+				break;
+
+			case '?':
+			default:
+				System.err.println( "Usage: " + NQueensProblemCountBitFlagsRecursive.class.getSimpleName() + " [-n <size of the chessboard>]" );
+				System.exit( 1 );
+			}
+
+			c = getOpt.getopt();
+		}
+
+		new NQueensProblemCountBitFlagsRecursive( chessBoardSize );
+	}
+}
