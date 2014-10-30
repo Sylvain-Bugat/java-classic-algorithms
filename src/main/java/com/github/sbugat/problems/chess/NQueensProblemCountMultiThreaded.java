ce5c61a6e8100dca1db8349e33eeefa6e514ace5
Sylvain Bugat <sylvain.bugat@laposte.net>
2014-10-30 21:41:03 +0100

    Revert multi-thread test

--- src/main/java/com/github/sbugat/problems/chess/NQueensProblemCountMultiThreaded.java
+++ src/main/java/com/github/sbugat/problems/chess/NQueensProblemCountMultiThreaded.java
@@ -1,8 +1,10 @@
 package com.github.sbugat.problems.chess;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -27,31 +29,33 @@ public class NQueensProblemCountMultiThreaded implements Runnable{
 	private final int chessboardSize;
 	private final int chessboardSizeMinusTwo;
 
-	private final boolean printSolutions = false;
+	private final boolean printSolutions;
 
-	private final int bitFlags;
 	private final int [] bitFlagsStack;
-	//private final int x;
+	private final int x;
 	private int bitFlagsMask;
 	private final int [] unusedColumnsStack;
 	private final int [] unusedAscendingDiagonalsStack;
 	private final int [] unusedDescendingDiagonalsStack;
-	private int stacklevel=1;
+	private int stacklevel=0;
 
-	public NQueensProblemCountMultiThreaded( final int chessboardSizeArg, final int bitFlagsArg, final int [] bitFlagsStackArg, final int [] unusedColumnsStackArg, final int [] unusedAscendingDiagonalsStackArg, final int [] unusedDescendingDiagonalsStackArg ) {
+	public NQueensProblemCountMultiThreaded( final int chessboardSizeArg, final int xArg, final boolean printSolutionsArg ) {
 
 		chessboardSize = chessboardSizeArg;
 		chessboardSizeMinusTwo = chessboardSizeArg - 2;
-		bitFlags = bitFlagsArg;
+		x = xArg;
 
-		bitFlagsStack = Arrays.copyOf( bitFlagsStackArg, bitFlagsStackArg.length );
-		unusedColumnsStack = Arrays.copyOf( unusedColumnsStackArg, unusedColumnsStackArg.length );
-		unusedAscendingDiagonalsStack = Arrays.copyOf( unusedAscendingDiagonalsStackArg, unusedAscendingDiagonalsStackArg.length );
-		unusedDescendingDiagonalsStack = Arrays.copyOf( unusedDescendingDiagonalsStackArg, unusedDescendingDiagonalsStackArg.length );
+		printSolutions = printSolutionsArg;
+
+		bitFlagsStack = new int[ chessboardSizeArg ];
 
 		for(int i=0 ; i < chessboardSizeArg ; i++ ) {
 			bitFlagsMask |= 1 << i;
 		}
+
+		unusedColumnsStack = new int[ chessboardSizeArg ];
+		unusedAscendingDiagonalsStack = new int[ chessboardSizeArg ];
+		unusedDescendingDiagonalsStack = new int[ chessboardSizeArg ];
 	}
 
 	/**
@@ -59,7 +63,42 @@ public class NQueensProblemCountMultiThreaded implements Runnable{
 	 */
 	public void run() {
 
-		solve( bitFlags );
+		//Test half square of the line
+		if( x < chessboardSize/2 ){
+
+			unusedColumnsStack[ 0 ] = 1 << x;
+			unusedAscendingDiagonalsStack[ 0 ] = ( 1 << x ) << 1;
+			unusedDescendingDiagonalsStack[ 0 ] = ( 1 << x ) >> 1;
+			final int bitFlags = bitFlagsMask & ~( unusedColumnsStack[ 0 ] | unusedAscendingDiagonalsStack[ 0 ] | unusedDescendingDiagonalsStack[ 0 ] );
+			bitFlagsStack[ 0 ] = bitFlags;
+
+			//Go on to the second line
+			stacklevel = 0;
+			solve( bitFlags );
+		}
+		//If the cheesboard size is odd, test with a queen on the middle of the first line
+		else if( x == chessboardSize / 2 ) {
+
+			final int x=chessboardSize/2;
+
+			unusedColumnsStack[ 0 ] = 1 << x;
+			unusedAscendingDiagonalsStack[ 0 ] = ( 1 << x ) << 1;
+			unusedDescendingDiagonalsStack[ 0 ] = ( 1 << x ) >> 1;
+
+			//just test next line half of possible position because or mirroring
+			int bitFlags = 0; //bitFlagsMask & ~( unusedColumnsStack[ 0 ] | unusedAscendingDiagonalsStack[ 0 ] | unusedDescendingDiagonalsStack[ 0 ] );
+			for(int i=0 ; i < x - 1 ; i++ ) {
+				bitFlags ^= 1 << i;
+			}
+			bitFlagsStack[ 0 ] = bitFlags;
+
+			//Go on to the second line
+			stacklevel = 0;
+			solve( bitFlags );
+		}
+
+		//Multiply by 2 the solution count for the other half not calculated
+		solutionCount *= 2;
 	}
 
 	/**
@@ -115,7 +154,7 @@ public class NQueensProblemCountMultiThreaded implements Runnable{
 			while( bitFlags == 0 ) {
 
 				//If there is still something to unstack
-				if( stacklevel > 1 ) {
+				if( stacklevel > 0 ) {
 					//Backtrace process
 					bitFlags = bitFlagsStack[ stacklevel ];
 					stacklevel --;
@@ -150,133 +189,6 @@ public class NQueensProblemCountMultiThreaded implements Runnable{
 		System.out.println( line + "    " + StringUtils.reverse( line ) );
 	}
 
-
-	private static void initSolve( final List<NQueensProblemCountMultiThreaded> threadList, final int chessboardSize) {
-
-		final int [] bitFlagsStack = new int[ chessboardSize ];
-		int bitFlagsMask = 0;
-		final int [] unusedColumnsStack = new int[ chessboardSize ];;
-		final int [] unusedAscendingDiagonalsStack = new int[ chessboardSize ];
-		final int [] unusedDescendingDiagonalsStack = new int[ chessboardSize ];
-
-		for(int i=0 ; i < chessboardSize ; i++ ) {
-			bitFlagsMask |= 1 << i;
-		}
-
-		//Test half square of the line
-		for( int x=0 ; x < chessboardSize/2 ; x ++ ){
-
-			unusedColumnsStack[ 0 ] = 1 << x;
-			unusedAscendingDiagonalsStack[ 0 ] = ( 1 << x ) << 1 ;
-			unusedDescendingDiagonalsStack[ 0 ] = ( 1 << x ) >> 1;
-			final int bitFlags = bitFlagsMask & ~( unusedColumnsStack[ 0 ] | unusedAscendingDiagonalsStack[ 0 ] | unusedDescendingDiagonalsStack[ 0 ] );
-			bitFlagsStack[ 0 ] = bitFlags;
-
-			//Go on to the second line
-			initSolve( threadList, bitFlags, 0, chessboardSize - 2, bitFlagsMask, bitFlagsStack, unusedColumnsStack, unusedAscendingDiagonalsStack, unusedDescendingDiagonalsStack );
-		}
-
-		//If the cheesboard size is odd, test with a queen on the middle of the first line
-		if( 0 != chessboardSize % 2 ) {
-
-			final int x=chessboardSize/2;
-
-			unusedColumnsStack[ 0 ] = 1 << x;
-			unusedAscendingDiagonalsStack[ 0 ] = ( 1 << x ) << 1;
-			unusedDescendingDiagonalsStack[ 0 ] = ( 1 << x ) >> 1;
-
-			//just test next line half of possible position because or mirroring
-			int bitFlags = 0; //bitFlagsMask & ~( unusedColumnsStack[ 0 ] | unusedAscendingDiagonalsStack[ 0 ] | unusedDescendingDiagonalsStack[ 0 ] );
-			for(int i=0 ; i < x - 1 ; i++ ) {
-				bitFlags ^= 1 << i;
-			}
-			bitFlagsStack[ 0 ] = bitFlags;
-
-			//Go on to the second line
-			initSolve( threadList, bitFlags, 0, chessboardSize - 2, bitFlagsMask, bitFlagsStack, unusedColumnsStack, unusedAscendingDiagonalsStack, unusedDescendingDiagonalsStack );
-		}
-	}
-
-	/**
-	 * Solving with iterative/stacking method by using bit flags, do a depth-first/back-tracking algorithm
-	 * a queen must me placed on the first line
-	 *
-	 * @param initial bitFlag with a single queen on the first line
-	 */
-	private static void initSolve( final List<NQueensProblemCountMultiThreaded> threadList, int bitFlags, int stacklevel, final int chessboardSizeMinusTwo, final int bitFlagsMask, final int [] bitFlagsStack, final int [] unusedColumnsStack, final int [] unusedAscendingDiagonalsStack, final int [] unusedDescendingDiagonalsStack ) {
-
-		int prevStacklevel = stacklevel - 1;
-		int targetQueen;
-		//Infinite loop, exit condition is tested when unstacking a queen
-		while( true ) {
-
-			//Test first possible queen of the line using direct inlining(manual code copy) of this method call: Integer.lowestOneBit( bitFlags );
-			//if the row is not already blocked by another queen and if both diagonals are not already blocked by anothers queens
-			//Don't need to test if targetQueen is not 0 because bitFlags has not been unstacked at the end of the loop (=contain at least one 0)
-			targetQueen = -( bitFlags ) & ( bitFlags );
-
-			//All queens are sets on the chessboard then a solution is found!
-			//Test with the board size minus 2 because the targeted queen is not placed yet
-			if( stacklevel >= 0 ) {
-
-				//Go on to the next line
-				prevStacklevel = stacklevel++;
-				//Mark the current target queen as tested for this stack level
-				bitFlagsStack[ stacklevel ] = bitFlags ^ targetQueen;
-
-				//unusedColumnsStack[ stacklevel ] = unusedColumnsStack[ prevStacklevel ] | targetQueen;
-				//unusedAscendingDiagonalsStack[ stacklevel ] = ( unusedAscendingDiagonalsStack[ prevStacklevel ] | targetQueen ) << 1;
-				//unusedDescendingDiagonalsStack[ stacklevel ] = ( unusedDescendingDiagonalsStack[ prevStacklevel ] | targetQueen ) >> 1;
-				//bitFlags = bitFlagsMask & ( unusedColumnsStack[ stacklevel ] | unusedAscendingDiagonalsStack[ stacklevel ] | unusedDescendingDiagonalsStack[ stacklevel ] );
-
-				//Update bit flags and do 3 stacks updates (4 previous commented lines in 1)
-				int nextBitFlags = bitFlagsMask & ~( ( unusedColumnsStack[ stacklevel ] = unusedColumnsStack[ prevStacklevel ] | targetQueen )
-						| ( unusedAscendingDiagonalsStack[ stacklevel ] = ( unusedAscendingDiagonalsStack[ prevStacklevel ] | targetQueen ) << 1 )
-						| ( unusedDescendingDiagonalsStack[ stacklevel ] = ( unusedDescendingDiagonalsStack[ prevStacklevel ] | targetQueen ) >> 1 )
-						);
-
-				threadList.add( new NQueensProblemCountMultiThreaded( bitFlagsStack.length, nextBitFlags, bitFlagsStack, unusedColumnsStack, unusedAscendingDiagonalsStack, unusedDescendingDiagonalsStack ) );
-
-				stacklevel--;
-
-				bitFlags ^= targetQueen;
-			}
-			else {
-
-				//Go on to the next line
-				prevStacklevel = stacklevel++;
-				//Mark the current target queen as tested for this stack level
-				bitFlagsStack[ stacklevel ] = bitFlags ^ targetQueen;
-
-				//unusedColumnsStack[ stacklevel ] = unusedColumnsStack[ prevStacklevel ] | targetQueen;
-				//unusedAscendingDiagonalsStack[ stacklevel ] = ( unusedAscendingDiagonalsStack[ prevStacklevel ] | targetQueen ) << 1;
-				//unusedDescendingDiagonalsStack[ stacklevel ] = ( unusedDescendingDiagonalsStack[ prevStacklevel ] | targetQueen ) >> 1;
-				//bitFlags = bitFlagsMask & ( unusedColumnsStack[ stacklevel ] | unusedAscendingDiagonalsStack[ stacklevel ] | unusedDescendingDiagonalsStack[ stacklevel ] );
-
-				//Update bit flags and do 3 stacks updates (4 previous commented lines in 1)
-				bitFlags = bitFlagsMask & ~( ( unusedColumnsStack[ stacklevel ] = unusedColumnsStack[ prevStacklevel ] | targetQueen )
-						| ( unusedAscendingDiagonalsStack[ stacklevel ] = ( unusedAscendingDiagonalsStack[ prevStacklevel ] | targetQueen ) << 1 )
-						| ( unusedDescendingDiagonalsStack[ stacklevel ] = ( unusedDescendingDiagonalsStack[ prevStacklevel ] | targetQueen ) >> 1 )
-						);
-			}
-
-			//If all positions have been tested or are already blocked by a column or a diagonal
-			while( bitFlags == 0 ) {
-
-				//If there is still something to unstack
-				if( stacklevel > 0 ) {
-					//Backtrace process
-					bitFlags = bitFlagsStack[ stacklevel ];
-					stacklevel --;
-				}
-				//Exit if all possibilities are tested
-				else {
-					return;
-				}
-			}
-		}
-	}
-
 	/**
 	 * Prepare and run all thread with a fixed thread pool
 	 *
@@ -286,38 +198,40 @@ public class NQueensProblemCountMultiThreaded implements Runnable{
 	 */
 	public static void launchMultiThread( final int chessBoardSize, final boolean printSolutions, final int threadNumber ) {
 
+		final ExecutorService executorService = Executors.newFixedThreadPool( threadNumber );
+
+		final List<NQueensProblemCountMultiThreaded> listeThread = new ArrayList<>();
+
 		//Prepare a thread for each possible possition on the first half of the first line
-		initSolve( ThreadLauncher.threadList, chessBoardSize );
+		for( int x=0 ; x < chessBoardSize/2 ; x ++ ){
 
-		final List<NQueensProblemCountMultiThreaded> threadList = new ArrayList<>( ThreadLauncher.threadList );
-
-		final List<Thread> launchedThreadList = new ArrayList<>();
-		System.out.println( ThreadLauncher.threadList.size() );
-		for( int i=0 ; i< threadNumber ; i++ ) {
-
-			final Thread thread = new Thread( new ThreadLauncher() );
-			thread.start();
-
-			launchedThreadList.add( thread );
+			final NQueensProblemCountMultiThreaded thread = new NQueensProblemCountMultiThreaded( chessBoardSize, x, printSolutions );
+			listeThread.add( thread );
+			executorService.execute( thread );
 		}
 
-		for( final Thread thread : launchedThreadList ) {
-			try {
-				thread.join();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		//If the size of the board is odd, prepare a thread with a queen in the middle of the first line
+		if( 0 != chessBoardSize % 2 ) {
+
+			final NQueensProblemCountMultiThreaded thread = new NQueensProblemCountMultiThreaded( chessBoardSize, chessBoardSize / 2, printSolutions );
+			listeThread.add( thread );
+			executorService.execute( thread );
+		}
+
+		try {
+			executorService.shutdown();
+			executorService.awaitTermination( 1,  TimeUnit.DAYS );
+		}
+		catch ( final InterruptedException e) {
+			e.printStackTrace();
+			System.exit( 1 );
 		}
 
 		int solutionCount = 0;
-		for( final NQueensProblemCountMultiThreaded thread : threadList ) {
+		for( final NQueensProblemCountMultiThreaded thread : listeThread ) {
 
 			solutionCount += thread.solutionCount;
 		}
-
-		//Multiply by 2 the solution count for the other half not calculated
-		solutionCount *= 2;
 
 		System.out.println( "Total number of solution(s):" + solutionCount );
 	}
@@ -390,42 +304,5 @@ public class NQueensProblemCountMultiThreaded implements Runnable{
 		}
 
 		launchMultiThread( chessBoardSize, printSolutions, threadNumber );
-	}
-
-	private static class ThreadLauncher implements Runnable {
-
-		static final List<NQueensProblemCountMultiThreaded> threadList = new ArrayList<>();
-
-		@Override
-		public void run() {
-
-			Runnable thread = getNextThread();
-			while( null != thread) {
-
-				final Thread launchedThread = new Thread( thread );
-				launchedThread.start();
-
-				try {
-					launchedThread.join();
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-				thread = getNextThread();
-			}
-		}
-
-		public static synchronized Runnable getNextThread() {
-
-			if( ! threadList.isEmpty() ) {
-				final Runnable nextThread = threadList.get( 0 );
-
-				threadList.remove( 0 );
-				return nextThread;
-			}
-
-			return null;
-		}
 	}
 }
